82fd593170e526c3542b50bfa3a71a9b5c1d5a68
kritiaggarwal4 <kritiaggarwal456@gmail.com>
2026-08-24 00:16:02 +0530

    Time: 3 ms (15.64%), Space: 26.9 MB (37.09%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        stack<int> st;   // store indices
+        int water = 0;
+
+        for (int i = 0; i < height.size(); i++) {
+
+            while (!st.empty() && height[i] > height[st.top()]) {
+
+                int bottom = st.top();
+                st.pop();
+
+                if (st.empty()) //at the end no water is collected
+                
+                    break;
+
+                int left = st.top();
+
+                int width = i - left - 1;
+
+                int h = min(height[left], height[i])
+                        - height[bottom];
+
+                water += width * h;
+            }
+
+            st.push(i);
+        }
+
+        return water;
+    }
+};
