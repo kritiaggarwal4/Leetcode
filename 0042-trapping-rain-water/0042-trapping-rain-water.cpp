766c4a21fd8b1dc0fd4ab640ac59449306116094
kritiaggarwal4 <kritiaggarwal456@gmail.com>
2026-08-24 12:19:27 +0530

    Time: 1 ms (22.41%), Space: 27.1 MB (33.4%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        stack<int> st;   // store indices
-        int water = 0;
-
-        for (int i = 0; i < height.size(); i++) {
-
-            while (!st.empty() && height[i] > height[st.top()]) {
-
-                int bottom = st.top();
-                st.pop();
-
-                if (st.empty()) //at the end no water is collected
-                
-                    break;
-
-                int left = st.top();
-
-                int width = i - left - 1;
-
-                int h = min(height[left], height[i])
-                        - height[bottom];
-
-                water += width * h;
-            }
-
-            st.push(i);
-        }
-
-        return water;
+        int n = height.size();
+        if(n<=2)
+        return 0;
+        vector<int>left_max(n);
+        vector<int>right_max(n);
+     left_max[0] = height[0];
+     for(int i=1;i<n;i++){
+        left_max[i] = max(left_max[i-1], height[i]);
+     }
+     right_max[n-1] = height[n-1];
+     for(int i=n-2;i>=0;i--)
+{
+    right_max[i] = max(right_max[i+1], height[i]);
+}
+int trapped_water = 0;
+for(int i=1;i<n-1;i++){
+    trapped_water += min(left_max[i], right_max[i]) - height[i];
+}
+return trapped_water;
     }
 };
