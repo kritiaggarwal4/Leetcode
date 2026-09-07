46d9694a2cedd17a028c640a31de31ca885c53ee
kritiaggarwal4 <kritiaggarwal456@gmail.com>
2026-09-07 21:53:07 +0530

    Time: 31 ms (53.25%), Space: 44.4 MB (11.46%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
-int solve(vector<vector<int>>&dp, string &s, string &t, int m, int n){
+// int solve(vector<vector<int>>&dp, string &s, string &t, int m, int n){
 
-    if(n ==0){
-        return 1;
-    }
-    if(m ==0){
-        return 0;
-    }
-    if(dp[n][m]!=-1){
-        return dp[n][m];
-    }
-    if(s[m-1]== t[n-1]){
-        return dp[n][m] = solve(dp, s, t, m-1, n-1)+ solve(dp, s,t, m-1, n);
-    }
-    else{
-        return dp[n][m]= solve(dp, s, t, m-1, n);
-    }
-}
+//     if(n ==0){
+//         return 1;
+//     }
+//     if(m ==0){
+//         return 0;
+//     }
+//     if(dp[n][m]!=-1){
+//         return dp[n][m];
+//     }
+//     if(s[m-1]== t[n-1]){
+//         return dp[n][m] = solve(dp, s, t, m-1, n-1)+ solve(dp, s,t, m-1, n);
+//     }
+//     else{
+//         return dp[n][m]= solve(dp, s, t, m-1, n);
+//     }
+// }
     int numDistinct(string s, string t) {
-      int m = s.length();
-      int n = t.length();
-      vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-      return solve(dp, s, t, m ,n);
+       int m = s.length();
+        int n = t.length();
+
+        vector<vector<long long>> dp(m + 1, vector<long long>(n + 1, 0));
+
+        // Empty t can be formed in exactly 1 way
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+
+        // Empty s cannot form a non-empty t
+        for(int j = 1; j <= n; j++) {
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+
+                if(s[i-1] == t[j-1]) {
+                    dp[i][j] = min(1LL * INT_MAX,
+                   dp[i-1][j-1] + dp[i-1][j]);
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
+     // return solve(dp, s, t, m ,n);
     }
 };
